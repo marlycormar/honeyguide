f660c2b9413debff3586900893e44e311ca882f1
Marly Cormar <marlycormar@gmail.com>
2018-04-26 04:41:46 -0400

    Remove reference to hcvprod in quail_user_script.sh

--- quail_user_script.sh
+++ quail_user_script.sh
@@ -2,4 +2,4 @@
 # note that for some reason it HAS to come before the c flag
 # I couldn't find this documented anywhere
 echo "Starting quail pull"
-su -mc 'bash /home/hcvprod/quail_run_script.sh' - hcvprod
+su -mc 'bash /home/icare/quail_run_script.sh' - icare
