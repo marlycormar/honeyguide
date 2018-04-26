952f9ecc7e36ad7c59f2671203e809b48e8527a4
Marly Cormar <marlycormar@gmail.com>
2018-04-26 04:33:57 -0400

    Remove reference to hcvprod

--- quail_run_script.sh
+++ quail_run_script.sh
@@ -1,19 +1,19 @@
-cd /home/hcvprod/quailroot
+cd /home/icare/quailroot
 echo $(whoami) $(pwd)
 
-if [ ! -d "/home/hcvprod/quailroot/sources/hcvprod" ]; then
-    printf "No %s project defined, building the project from environment variables." hcvprod | echo
+if [ ! -d "/home/icare/quailroot/sources/icare" ]; then
+    printf "No %s project defined, building the project from environment variables." icare | echo
     echo "This is stored in a volume on the machine! Remember to clean up!"
-    quail redcap generate quail.conf.yaml hcvprod $TOKEN $REDCAP_URL
+    quail redcap generate quail.conf.yaml icare $TOKEN $REDCAP_URL
     echo "Done adding the quail redcap source"
 fi
 
 # Download the data
 echo "getting redcap metadata"
-quail redcap get_meta hcvprod
+quail redcap get_meta icare
 echo "getting redcap data"
-quail redcap get_data hcvprod
+quail redcap get_data icare
 echo "generating the redcap metadata database"
-quail redcap gen_meta hcvprod
+quail redcap gen_meta icare
 echo "generating the redcap data database"
-quail redcap gen_data hcvprod
+quail redcap gen_data icare
